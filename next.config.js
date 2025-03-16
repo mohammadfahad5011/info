a8a5bfec9ce7ce32539e6e9236013f11c0d2d6f2
Mohammad Fahad <85410237+mohammadfahad5011@users.noreply.github.com>
2025-03-16 18:48:18 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://info-tech-rho.vercel.app/api/:path*",
+        destination: "https://info-indol-nu.vercel.app/api/:path*",
       },
     ];
   },
